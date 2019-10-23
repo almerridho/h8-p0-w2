4a1d0515bccd879979f747c7dffd5ff112986395
Varian Almerridho <almerridho.99@Varians-MacBook-Air.local>
2019-10-23 14:45:18 +0700

    first commit

---
+++ tugas-if-else.js
@@ -0,0 +1,12 @@
+var name = "Steve"
+var role = "warrior"
+
+if(role === "healer"){
+    console.log("Welcome to Proxytia " + name + " the Healer!")
+}else if(role === "mage"){
+    console.log("Welcome to Proxytia " + name + " the Mage!")
+}else if(role === "warrior"){
+    console.log("Welcome to Proxytia " + name + " the Warrior!")
+}else if(role === "archer"){
+    console.log("Welcome to Proxytia " + name + " the Archer!")
+}
