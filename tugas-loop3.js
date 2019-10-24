ae192e040679c9f52bf8b744744233adacb4bdb0
almerridho <56529424+almerridho@users.noreply.github.com>
2019-10-24 17:01:09 +0700

    Add files via upload

---
+++ tugas-loop3.js
@@ -0,0 +1,32 @@
+var i = 1
+
+for (i = 1; i <= 100; i++){
+  if (i % 2 === 0){
+    console.log("GENAP");
+  }else {
+    console.log("GANJIL");
+  }
+    
+}
+
+for (i = 1; i <= 100; i++){
+  if (i % 2 === 0){
+    console.log(i + " kelipatan 2");
+  }
+      
+}
+
+for (i = 1; i <= 100; i++){
+  if (i % 5 === 0){
+    console.log(i + " kelipatan 5");
+  }
+        
+}
+
+for (i = 1; i <= 100; i++){
+  if (i % 9 === 0){
+     console.log(i + " kelipatan 9");
+  }
+        
+}
+  
