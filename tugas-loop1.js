00144da09f41c29874713ba502feb430041c5f2f
almerridho <56529424+almerridho@users.noreply.github.com>
2019-10-24 12:35:37 +0700

    Add files via upload

---
+++ tugas-loop1.js
@@ -0,0 +1,16 @@
+var text = " - I love coding";
+var i = 2;
+
+while (i <=20) {
+    console.log(i + text);
+    i = i + 2;
+   
+}
+
+var text2 = " - I will become a fullstack developer";
+var j = 20;
+
+while (j>=2) {
+    console.log(j + text2);
+    j = j - 2;
+}
