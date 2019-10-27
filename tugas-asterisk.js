6581f0299a48d34c536bce870cac22a4811361e6
almerridho <56529424+almerridho@users.noreply.github.com>
2019-10-27 19:54:39 +0700

    Add files via upload

---
+++ tugas-asterisk.js
@@ -0,0 +1,19 @@
+//loop asterisk 1
+var i = 1
+for (i = 1; i <=5; i++){
+  console.log("*");
+}
+
+//loop asterisk 2
+var i = 1
+for (i = 1; i <=5; i++){
+  console.log("*****");
+}
+
+//loop asterisk 3
+var i = 1
+var kata = ''
+for (var i = 1; i <=5; i++){
+  kata = kata + "*"
+  console.log(kata)
+}
