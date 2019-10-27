f07ea92bdd3d2bd28de7379c23a151eba6abce77
almerridho <56529424+almerridho@users.noreply.github.com>
2019-10-27 21:14:17 +0700

    Add files via upload

---
+++ tugas-string-3dan4.js
@@ -0,0 +1,17 @@
+//no 3
+var word3 = 'wow JavaScript is so cool';
+
+console.log('First Word: ' + word3.substring(0, 3));
+console.log('Second Word: ' + word3.substring(4, 14));
+console.log('Third Word: ' + word3.substring(15, 17));
+console.log('Fourth Word: ' + word3.substring(18, 20));
+console.log('Fifth Word: ' + word3.substring(21, 25));
+
+//no 4
+var word3 = 'wow JavaScript is so cool';
+
+console.log('First Word: ' + word3.substring(0, 3) + ', with length: ' + word3.substring(0, 3).length);
+console.log('Second Word: ' + word3.substring(4, 14) + ', with length: ' + word3.substring(4, 14).length);
+console.log('Third Word: ' + word3.substring(15, 17) + ', with length: ' + word3.substring(15, 17).length);
+console.log('Fourth Word: ' + word3.substring(18, 20) + ', with length: ' + word3.substring(18, 20).length);
+console.log('Fifth Word: ' + word3.substring(21, 25) + ', with length: ' + word3.substring(21, 25).length);
